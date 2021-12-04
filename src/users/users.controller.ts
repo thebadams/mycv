31ef181d3042f302832739802acf86d794b91e3e
Brian Adams <bpeteradams@gmail.com>
2021-12-04 14:36:23 -0500

    edited users.service to work with user repository and wired it to users.cotnroller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
-
+import { UsersService } from './users.service';
 @Controller('auth')
 export class UsersController {
+	constructor(private usersService: UsersService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    this.usersService.create(body.email, body.password);
   }
 }
